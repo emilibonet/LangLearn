00d24de322ac7c87efbabb8ecc0cb43ee6f8369b
emili.bonet <emili.bonet@viume.co>
2023-09-13 16:56:01 +0200

    Updated lang checking and added for mode.

--- main.py
+++ main.py
@@ -22,9 +22,11 @@ def main(args:argparse.Namespace) -> None:
 
 
 if __name__ == "__main__":
+    available_languages = [lang.lower() for lang in os.listdir(f'{root_dir}/Material')]
+    available_modes = ['conjugations', 'vocabulary']
     parser = argparse.ArgumentParser()
-    parser.add_argument("--lang", help="Select language to be practiced.", type=str, choices=['french', 'german', 'occitan'])
-    parser.add_argument("--mode", help="Determines what the quiz will be about.", type=str, choices=['conjugations', 'vocabulary'])
+    parser.add_argument("--lang", help="Select language to be practiced.", type=str, choices=available_languages)
+    parser.add_argument("--mode", help="Determines what the quiz will be about.", type=str, choices=available_modes)
     parser.add_argument("--save", help="Record results from this quiz.", action='store_true', default=False)
     parser.add_argument("--user", help="User where the records will be saved.", type=str, default=None)
     parser.add_argument("--word", help="Ask this word/verb in particular.", type=str, default=None)
@@ -33,12 +35,19 @@ if __name__ == "__main__":
 
     # Check arguments.
     if args.lang is None:
-        raise ValueError(f"ERROR -- You must specify a language with the '--lang <LANG>' flag.")
-    if args.lang not in os.listdir(f'{root_dir}/Material'):
-        separator = "', '"
-        raise ValueError(f"ERROR -- Language '{args.lang}' not found. Available languages are '{separator.join(os.listdir(f'{root_dir}/Material'))}'.")
+        raise ValueError(f"ERROR -- You must specify a language with the flag '--lang <LANG>'.")
+    separator = "', '"
+    if args.lang not in available_languages:
+        raise ValueError(f"ERROR -- Language '{args.lang}' not found. Available languages are '{separator.join(available_languages)}'.")
+    
+    if args.mode is None:
+        raise ValueError(f"ERROR -- You must specify a mode with the flag '--mode <MODE>'.")
+    if args.mode not in available_modes:
+        raise ValueError(f"ERROR -- Mode '{args.mode}' not found. Available languages are '{separator.join(available_modes)}'.")
+    
     if args.save and args.user is not None:
         raise ValueError("ERROR -- Need to provide a user if you want to save.")
+    
     if args.word is not None and args.common:
         args.common = False
         raise ValueError("WARNING -- The --common flag is ignored if a word is given.")
